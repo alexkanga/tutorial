19571bc76495e02230d5626ee2eca22a914e66e1
alexkanga <kalexane@gmail.com>
2023-11-08 17:57:32 +0000

    end of tutorial nodejs postgres render

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+itemsPool = require('./dbConfig');
+
+app.get('/', (req, res) => {
+    res.send('Simple API page Homepage')
+})
+
+app.get('/api/items', async(req, res) => {
+    try {
+        const allItems = await itemsPool.query(
+            'SELECT * FROM items'
+        );
+        const {allItemsRows} = allItems;
+        // res.json({ allItemsRows });
+        res.json({items: allItems.rows });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message)
+    }
+})
+app.post('/api/items', async (req, res) => {
+    const { description } = req.body;
+    try {
+        const newItem = await itemsPool.query(
+            'INSERT INTO items (description) VALUES ($1) RETURNING *',
+            [description]
+        );
+        res.json({ 
+            message: "New item added!",
+            item: newItem.rows
+         });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message)
+    }
+})
+
+
+
+app.listen(5070, () => {
+    console.log('Server running on port 5070');
+}
+);
